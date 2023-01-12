2c384eac8efcae38e2c364a1a81accc394725520
stevenjhli <stevenjhli@tencent.com>
2023-01-12 17:57:15 +0800

    merge code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,31 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['standard', 'plugin:prettier/recommended'], // 望在保存的时候按照prettier的代码分格格式化代码。eslint-plugin-prettier
-  parser: '@typescript-eslint/parser',
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      // Your TypeScript files extension
-      // As mentioned in the comments, you should extend TypeScript plugins here,
-      // instead of extending them outside the `overrides`.
-      // If you don't want to extend any rules, you don't need an `extends` attribute.
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking'
-      ],
-      parserOptions: {
-        project: ['./tsconfig.json'] // Specify it only for TypeScript files
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    tsconfigRootDir: __dirname
-  },
-  rules: {}
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ["standard", "plugin:prettier/recommended"], // 望在保存的时候按照prettier的代码分格格式化代码。eslint-plugin-prettier
+	parser: "@typescript-eslint/parser",
+	overrides: [
+		{
+			files: ["*.ts", "*.tsx"],
+			// Your TypeScript files extension
+			// As mentioned in the comments, you should extend TypeScript plugins here,
+			// instead of extending them outside the `overrides`.
+			// If you don't want to extend any rules, you don't need an `extends` attribute.
+			extends: [
+				"plugin:prettier/recommended",
+				"plugin:@typescript-eslint/recommended",
+				"plugin:@typescript-eslint/recommended-requiring-type-checking"
+			],
+			parserOptions: {
+				project: ["./tsconfig.json"] // Specify it only for TypeScript files
+			}
+		}
+	],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		tsconfigRootDir: __dirname
+	},
+	rules: {}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Koa, { DefaultContext, DefaultState, Context } from "koa"
-
 // import dotenv from 'dotenv'
 // dotenv.config()
 // const { PORT, HOST } = process.env
