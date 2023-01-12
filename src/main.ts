dc5b7c59602935668d29c33d89c825f674bd310d
stevenjhli <stevenjhli@tencent.com>
2023-01-12 19:52:01 +0800

    fix: remove duplicated code segment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
 import Koa, { DefaultContext, DefaultState, Context } from 'koa'
-// import dotenv from 'dotenv'
-// dotenv.config()
-// const { PORT, HOST } = process.env
 
 const app: Koa<DefaultState, DefaultContext> = new Koa()
 
