df5b7d66a76099508c87416bf983a5db0c3930b2
stevenjhli <stevenjhli@tencent.com>
2023-01-13 11:30:48 +0800

    add: replenish build command

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import Koa, { DefaultContext, DefaultState, Context } from 'koa'
 const app: Koa<DefaultState, DefaultContext> = new Koa()
 
 app.use((ctx: Context) => {
-	ctx.body = 'coderlzw'
+	ctx.body = 'Hello World'
 })
 
 app.listen(3000, () => {
-	console.log('Server serving at http://127.0.0.1:3000')
+	console.log('Server is running at http://127.0.0.1:3000')
 })
